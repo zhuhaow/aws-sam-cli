c30c8a6e815617096463995abd8e2143bd4aa090
Mehmet Nuri Deveci <5735811+mndeveci@users.noreply.github.com>
2021-11-22 19:19:52 -0800

    check failure

--- tests/unit/lib/intrinsic_resolver/test_intrinsic_resolver.py
+++ tests/unit/lib/intrinsic_resolver/test_intrinsic_resolver.py
@@ -1075,8 +1075,9 @@ class TestResolveTemplate(TestCase):
     def load_test_data(self, template_path):
         integration_path = str(Path(__file__).resolve().parents[0].joinpath("test_data", template_path))
         region_name = boto3.session.Session().region_name or "us-east-1"
+        zone_name = boto3.client('ec2').describe_availability_zones().get("AvailabilityZones")[0]("ZoneName")
         with open(integration_path) as f:
-            template = json.loads(f.read().replace("us-east-1", region_name))
+            template = json.loads(f.read().replace("us-east-1a", zone_name).replace("us-east-1", region_name))
         return template
 
     @parameterized.expand(
